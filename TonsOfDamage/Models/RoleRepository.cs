2c0d987145fa8297631b9616812438e7f56e582e
Fynzie <fynz@hotmail.fr>
2018-02-20 22:28:19 +0100

    first commit

---
+++ TonsOfDamage/Models/RoleRepository.cs
@@ -0,0 +1,122 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TonsOfDamage.Models
+{
+    public class RoleRepository : IRoleStore<Role>
+    {
+        private readonly string _connectionString;
+
+        public RoleRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task<IdentityResult> CreateAsync(Role role, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync(cancellationToken);
+                role.Id = await connection.QuerySingleAsync<int>($@"INSERT INTO [Role] ([Name], [NormalizedName])
+                VALUES (@{nameof(Role.Name)}, @{nameof(Role.NormalizedName)});
+                SELECT CAST(SCOPE_IDENTITY() as int)", role);
+            }
+
+            return IdentityResult.Success;
+        }
+
+        public async Task<IdentityResult> UpdateAsync(Role role, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync(cancellationToken);
+                await connection.ExecuteAsync($@"UPDATE [Role] SET
+                [Name] = @{nameof(Role.Name)},
+                [NormalizedName] = @{nameof(Role.NormalizedName)}
+                WHERE [Id] = @{nameof(Role.Id)}", role);
+            }
+
+            return IdentityResult.Success;
+        }
+
+        public async Task<IdentityResult> DeleteAsync(Role role, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync(cancellationToken);
+                await connection.ExecuteAsync($"DELETE FROM [Role] WHERE [Id] = @{nameof(Role.Id)}", role);
+            }
+
+            return IdentityResult.Success;
+        }
+
+        public Task<string> GetRoleIdAsync(Role role, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(role.Id.ToString());
+        }
+
+        public Task<string> GetRoleNameAsync(Role role, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(role.Name);
+        }
+
+        public Task SetRoleNameAsync(Role role, string roleName, CancellationToken cancellationToken)
+        {
+            role.Name = roleName;
+            return Task.FromResult(0);
+        }
+
+        public Task<string> GetNormalizedRoleNameAsync(Role role, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(role.NormalizedName);
+        }
+
+        public Task SetNormalizedRoleNameAsync(Role role, string normalizedName, CancellationToken cancellationToken)
+        {
+            role.NormalizedName = normalizedName;
+            return Task.FromResult(0);
+        }
+
+        public async Task<Role> FindByIdAsync(string roleId, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync(cancellationToken);
+                return await connection.QuerySingleOrDefaultAsync<Role>($@"SELECT * FROM [Role]
+                WHERE [Id] = @{nameof(roleId)}", new { roleId });
+            }
+        }
+
+        public async Task<Role> FindByNameAsync(string normalizedRoleName, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync(cancellationToken);
+                return await connection.QuerySingleOrDefaultAsync<Role>($@"SELECT * FROM [Role]
+                WHERE [NormalizedName] = @{nameof(normalizedRoleName)}", new { normalizedRoleName });
+            }
+        }
+
+        public void Dispose()
+        {
+            // Nothing to dispose.
+        }
+    }
+}
